69d9b6abf4779cf8aa872ef9d1b4d26eeacca078
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2022-05-24 15:11:44 +0900

    좋아요 뺌

--- commang/routes/page.js
+++ commang/routes/page.js
@@ -34,7 +34,7 @@ router.get('/', async (req, res, next) => {
       order: [['createdAt', 'DESC']]
     });
     res.render('main', {
-      title: 'cex',
+      title: 'commang',
       twits: posts,
       comments:comments,
     });
