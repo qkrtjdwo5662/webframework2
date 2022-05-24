7b50a66da84ebd0c33580c96f06a24fb0d59a78c
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2022-05-25 02:36:58 +0900

    팔로우 끊기 & 좋아요 자기것도 가능

--- commang/routes/user.js
+++ commang/routes/user.js
@@ -20,6 +20,21 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   }
 });
 
+router.post('/:id/unfollow', isLoggedIn, async(req,res,next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (user) {
+      await user.removeFollowing(parseInt(req.params.id, 10));
+      res.send('success');
+    } else {
+      res.status(404).send('no user');
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 router.post('/change', isLoggedIn, async (req, res, next) => {
 
   const changeName = req.body.name;
